eb90cd6d85244f9ff236178feb206d33b935083c
patilvishal0597 <patilvishal0597@gmail.com>
2022-12-07 18:49:35 -0500

    UI changes

--- ui/src/App.js
+++ ui/src/App.js
@@ -7,20 +7,34 @@ import MessageParser from "./chatbot/MessageParser";
 import ActionProvider from "./chatbot/ActionProvider";
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
+import background from "./gradient.png"
+import "./App.css"
 
 function App() {
-  return (
-    <div className="App">
-      <Grid container direction="row" alignItems="center">
-        <Chatbot
-          config={config}
-          messageParser={MessageParser}
-          actionProvider={ActionProvider}
-        />
-        <Topics/>
-      </Grid>
-    </div>
-  );
+
+    const myStyle={
+      backgroundImage: `url(${background})`,
+      height:'100vh',
+      // marginTop:'-70px',
+      // fontSize:'50px',
+      backgroundSize: 'cover',
+      backgroundRepeat: 'no-repeat',
+    };
+    return (
+      <div style={myStyle}>
+        <div className="App">
+          <Grid container direction="row" alignItems="center" height="100vh" justifyContent="center">
+            <Chatbot
+              config={config}
+              messageParser={MessageParser}
+              actionProvider={ActionProvider}
+            />
+            <Topics/>
+          </Grid>
+        </div>
+      </div>
+    );
+  
 }
 
 export default App;
